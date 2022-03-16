55f28728d4f75223f94acfe1ed9138f295aafff5
Jun <jun.w4ng@gmail.com>
2022-03-16 15:27:42 -0300

    post agregado

--- db.js
+++ db.js
@@ -4,18 +4,32 @@ const pool = new Pool({
   connectionString: "postgresql://postgres:Junjie1995@localhost:5432/skatepark",
 });
 
-const queries = (text, values) => ({ text, values });
+const consulta = (text, values) => ({ text, values });
 
 //? Participantes
-const getParticipantes = () =>
-  pool.query("SELECT * FROM skaters").then((res) => res.rows);
+const getParticipantes = async () => {
+  const sqlQuery = "SELECT * FROM skaters";
+  try {
+    const result = await pool.query(sqlQuery);
+    return result.rows;
+  } catch (error) {
+    console.log(err.code);
+    return error;
+  }
+};
 
 //? Crea participante
-const postParticipantes = () =>
-  pool
-    .query(
-      "INSERT INTO skaters (email, nombre, password, anos_experencia, especialidad, foto, estado) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING*"
-    )
-    .then((res) => res.rows);
+const postParticipantes = async (data) => {
+  const sqlQuery =
+    "INSERT INTO skaters (email, nombre, password, anos_experiencia, especialidad, foto, estado) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING*";
+  const values = data;
+  try {
+    const result = await pool.query(consulta(sqlQuery, values));
+    return result.rows;
+  } catch (error) {
+    console.log(error.code);
+    return error;
+  }
+};
 
-module.exports = { getParticipantes };
+module.exports = { getParticipantes, postParticipantes };
--- routes/front.js
+++ routes/front.js
@@ -1,22 +1,38 @@
 const { Router } = require("express");
-const { getParticipantes } = require("../db");
+const { getParticipantes, postParticipantes } = require("../db");
+const bodyParser = require("body-parser");
 
 const router = Router();
 
-//! Hacer logica de estado
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+//! Hacer logica de estado (aprobado o en revision)
 //? Muestra participantes
 router.get("/", async (req, res) => {
   const data = await getParticipantes();
   res.render("index", { participantes: data });
 });
 
-router.get("/login", async (req, res) => {
-  //const {} = req.body
-  res.render("login");
-});
+//? Vistas
+router.get("/login", async (req, res) => res.render("login", req));
+router.get("/registro", async (req, res) => res.render("registro"));
 
-router.get("/registro", async (req, res) => {
-  res.render("registro");
+//? Registro de participante
+router.post("/register", async (req, res) => {
+  const { email, nombre, password, experiencia, especialidad, foto } = req.body;
+  const estado = "FALSE"; //? Setea default de estado
+  const data = {
+    email,
+    nombre,
+    password,
+    experiencia,
+    especialidad,
+    foto,
+    estado,
+  };
+  await postParticipantes(Object.values(data));
+  res.redirect('/')
 });
 
 router.get("/datos", (req, res) => {
